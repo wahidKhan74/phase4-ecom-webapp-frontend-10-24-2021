9c34b538176ec5361e72cda0293f5aa2c67f54af
Waheed Khan <wahid.khan74@gmail.com>
2021-10-24 11:34:08 +0000

    angular setup + boostratp + component + routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,35 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { UsersComponent } from './components/users/users.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { ProductsComponent } from './components/products/products.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
+import { ListComponent } from './components/products/list/list.component';
+import { CreateComponent } from './components/products/create/create.component';
+import { UpdateComponent } from './components/products/update/update.component';
+import { ViewComponent } from './components/products/view/view.component';
 
-const routes: Routes = [];
+//create routes
+const routes: Routes = [
+  { path: '', redirectTo:'/home',pathMatch:'full'},
+  { path: 'home', component: HomeComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'users', component: UsersComponent },
+  { path: 'signin', component: LoginComponent },
+  { path: 'signup', component: RegisterComponent },
+  { path: 'products', children:[
+    { path:'', component: ProductsComponent},
+    { path:'list', component: ListComponent},
+    { path:'create', component: CreateComponent},
+    { path:'update', component: UpdateComponent},
+    { path:'view', component: ViewComponent}   
+
+  ]},
+  { path: '**', component: NotFoundComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,38 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
+import { RegisterComponent } from './components/register/register.component';
+import { LoginComponent } from './components/login/login.component';
+import { AboutComponent } from './components/about/about.component';
+import { UsersComponent } from './components/users/users.component';
+import { ProductsComponent } from './components/products/products.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
+import { CreateComponent } from './components/products/create/create.component';
+import { ListComponent } from './components/products/list/list.component';
+import { UpdateComponent } from './components/products/update/update.component';
+import { ViewComponent } from './components/products/view/view.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    NavbarComponent,
+    HomeComponent,
+    RegisterComponent,
+    LoginComponent,
+    AboutComponent,
+    UsersComponent,
+    ProductsComponent,
+    NotFoundComponent,
+    CreateComponent,
+    ListComponent,
+    UpdateComponent,
+    ViewComponent
   ],
   imports: [
     BrowserModule,
